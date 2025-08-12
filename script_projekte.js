9f256d2f9334290f5ba49600df4b81fc659d87b8
DanielRendler97 <daniel.rendler97@web.de>
2025-08-12 18:02:36 +0200

    Quellenangaben

--- script_projekte.js
+++ script_projekte.js
@@ -5,19 +5,29 @@ const myCarousel = document.getElementById('carouselProjects');
 myCarousel.addEventListener('slide.bs.carousel', event => {
     if (event.to + 1 == 2){
         document.getElementById('description-1').style.display = 'none';
-        document.getElementById('description-2').style.display = 'block';}
+        document.getElementById('description-2').style.display = 'block';
+        document.getElementById('carousel-caption-img-1').classList.add('d-none');
+        document.getElementById('carousel-caption-img-2').classList.remove('d-none');}
     else if (event.to + 1 == 3){
         document.getElementById('description-2').style.display = 'none';
-        document.getElementById('description-3').style.display = 'block';}
+        document.getElementById('description-3').style.display = 'block';
+        document.getElementById('carousel-caption-img-2').classList.add('d-none');
+        document.getElementById('carousel-caption-img-3').classList.remove('d-none');}
     else if (event.to + 1 == 4){
         document.getElementById('description-3').style.display = 'none';
-        document.getElementById('description-4').style.display = 'block';}
+        document.getElementById('description-4').style.display = 'block';
+        document.getElementById('carousel-caption-img-3').classList.add('d-none');
+        document.getElementById('carousel-caption-img-4').classList.remove('d-none');}
     else if (event.to + 1 == 5){
         document.getElementById('description-4').style.display = 'none';
-        document.getElementById('description-5').style.display = 'block';}
+        document.getElementById('description-5').style.display = 'block';
+        document.getElementById('carousel-caption-img-4').classList.add('d-none');
+        document.getElementById('carousel-caption-img-5').classList.remove('d-none');}
     else if (event.to + 1 == 1){
         document.getElementById('description-5').style.display = 'none';
-        document.getElementById('description-1').style.display = 'block';}
+        document.getElementById('description-1').style.display = 'block';
+        document.getElementById('carousel-caption-img-5').classList.add('d-none');
+        document.getElementById('carousel-caption-img-1').classList.remove('d-none');}
 })
 
 
